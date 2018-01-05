568f331661f04c3cfb240a46249bf07e5955dc75
Leon <Leon@qq.com>
2018-01-05 19:41:57 +0800

    添加项目文件。

---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        // GET: Product
+        public ActionResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new List<Product>
+                {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Surf board", Price = 179 },
+                    new Product { Name = "Running shoes", Price = 95 }
+                });
+            kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
