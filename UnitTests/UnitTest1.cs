36be862775cd5f1cdac27697f108cd69558a36bb
Leon <Leon@qq.com>
2018-01-12 20:42:59 +0800

    使用新的路由方式，引用bootstrap，使用分部视图实现列表分页显示

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -6,6 +6,9 @@ using Domain.Abstract;
 using Domain.Entities;
 using WebUI.Controllers;
 using System.Linq;
+using WebUI.HtmlHelpers;
+using System.Web.Mvc;
+using WebUI.Models;
 
 namespace UnitTests
 {
@@ -13,7 +16,7 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
             //准备
             Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
@@ -28,13 +31,64 @@ namespace UnitTests
             controller.PageSize = 3;
 
             //动作
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
 
             //断言
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = myHelper.Pagelinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" +
+                            @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" +
+                            @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                            result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //准备
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" }
+            });
+
+            //准备
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //动作
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            //断言
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
