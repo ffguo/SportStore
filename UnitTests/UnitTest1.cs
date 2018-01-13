4b8fdfd5f044eae5d620ebecf028946f4c683aea
Leon <Leon@qq.com>
2018-01-13 19:53:21 +0800

    添加分类显示，修改url路由

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace UnitTests
             controller.PageSize = 3;
 
             //动作
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
 
             //断言
             Product[] prodArray = result.Products.ToArray();
@@ -81,7 +81,7 @@ namespace UnitTests
             controller.PageSize = 3;
 
             //动作
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
 
             //断言
             PagingInfo pageInfo = result.PagingInfo;
@@ -89,6 +89,33 @@ namespace UnitTests
             Assert.AreEqual(pageInfo.ItemsPerPage, 3);
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Product()
+        {
+            //准备
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+            });
+
+            //准备
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //动作
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            //断言
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
