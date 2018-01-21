df0f6e33c3d0c1d9bba4dd1b70bcedfd7ce856ef
Leon <851501783@qq.com>
2018-01-21 19:41:29 +0800

    添加购物车

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -165,5 +165,35 @@ namespace UnitTests
             //断言
             Assert.AreEqual(categoryToSelect, result);
         }
+
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //准备
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+            });
+
+            //准备
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            //动作
+            int res1 = ((ProductsListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductsListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductsListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductsListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+            //断言
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+        }
     }
 }
