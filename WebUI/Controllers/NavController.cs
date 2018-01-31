c4e466eb3a3554534ec12a8957e3e583c7c93e58
Leon <851501783@qq.com>
2018-01-31 20:31:34 +0800

    初步手机自适应

--- WebUI/Controllers/NavController.cs
+++ WebUI/Controllers/NavController.cs
@@ -16,7 +16,7 @@ namespace WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategory = category;
 
@@ -24,7 +24,9 @@ namespace WebUI.Controllers
                 .Select(x => x.Category)
                 .Distinct()
                 .OrderBy(x => x);
-            return PartialView(categories);
+
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
         }
     }
 }
